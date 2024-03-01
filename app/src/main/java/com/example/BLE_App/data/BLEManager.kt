1f1beb6f83f2b7815dce293a979264f9084d5f12
jimballl <kimball.j@northeastern.edu>
2024-02-29 19:16:05 -0500

    progress on bluetooh code

--- app/src/main/java/com/example/BLE_App/data/BLEManager.kt
+++ app/src/main/java/com/example/BLE_App/data/BLEManager.kt
@@ -1,6 +1,8 @@
 package com.example.BLE_App.data
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
@@ -16,61 +18,37 @@ class BLEManager(private val context: Context) {
     private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
     private var bluetoothGatt: BluetoothGatt? = null
 
+    companion object {
+        const val REQUEST_CODE_BLUETOOTH_PERMISSION = 1001 // example value
+    }
+
     private val gattCallback = object : BluetoothGattCallback() {
+        @SuppressLint("MissingPermission")
         override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
             when (newState) {
                 BluetoothProfile.STATE_CONNECTED -> {
-                    if (ActivityCompat.checkSelfPermission(
-                            context,
-                            Manifest.permission.BLUETOOTH_CONNECT
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return
-                    }
                     gatt.discoverServices()
+                }
+                BluetoothProfile.STATE_DISCONNECTED -> {
+                    bluetoothGatt = null
                 }
                 // Handle other states if necessary
             }
         }
 
+        @SuppressLint("MissingPermission")
         override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-            // UUIDs for the service and characteristic you're interested in
             val YOUR_SERVICE_UUID = UUID.fromString("00001810-0000-1000-8000-00805f9b34fb")
             val YOUR_CHARACTERISTIC_UUID = UUID.fromString("00002a35-0000-1000-8000-00805f9b34fb")
 
-            // Find the service and characteristic that correspond to the blood pressure reading
             val service = gatt.getService(YOUR_SERVICE_UUID)
             val characteristic = service?.getCharacteristic(YOUR_CHARACTERISTIC_UUID)
 
-
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
             gatt.readCharacteristic(characteristic)
         }
 
-        @Deprecated("Deprecated in Java")
         override fun onCharacteristicRead(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                // Convert the characteristic's value to a blood pressure reading
                 val bloodPressure = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16, 0)
                 // Do something with the blood pressure reading
             }
@@ -79,53 +57,27 @@ class BLEManager(private val context: Context) {
 
     fun connectToDevice(deviceAddress: String) {
         val device: BluetoothDevice? = bluetoothAdapter?.getRemoteDevice(deviceAddress)
-        if (ActivityCompat.checkSelfPermission(
+        if (context is Activity && ActivityCompat.checkSelfPermission(
                 context,
-                Manifest.permission.BLUETOOTH_CONNECT
+                Manifest.permission.BLUETOOTH_ADMIN
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+            ActivityCompat.requestPermissions(
+                context,
+                arrayOf(
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                REQUEST_CODE_BLUETOOTH_PERMISSION
+            )
+        } else {
+            bluetoothGatt = device?.connectGatt(context, false, gattCallback)
         }
-        bluetoothGatt = device?.connectGatt(context, false, gattCallback)
     }
 
+    @SuppressLint("MissingPermission")
     fun disconnectFromDevice() {
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
         bluetoothGatt?.disconnect()
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
         bluetoothGatt?.close()
         bluetoothGatt = null
     }
